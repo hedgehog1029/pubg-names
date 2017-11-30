0188dd1e576e872100f777224f9af25d99f119f0
Henry <omgredstone@gmail.com>
2017-11-30 20:50:19 +0000

    Actually added all the code

---
+++ static/index.js
@@ -0,0 +1,129 @@
+(function() {
+    var ele = document.querySelector("#generated");
+
+    function randomFrom(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
+
+    function p(val) {
+        return function() {
+            return val
+        }
+    }
+
+    function r(chance) {
+        if (!chance) chance = 0.5
+        return function() {
+            return Math.random() < chance
+        }
+    }
+
+    var pools = {
+        pairs: [
+            ["Brendan Greene's", ["Magic Machine", "Fight Ravine", "Sideways Lean", "Shine n' Gleam", "Coming Clean", "Magical Bean", "Bug Screen", "Gaming Scene", "Have You Seen?", "Party Bean"]],
+            ["Brenden Grenden's", ["Battle Grenden", "British Referendum", "Increased Momentum", "Physics Intervention"]],
+            ["Playerunknown's", ["Daily Moan", "Cordless Phone", "Gunplay Zone", "Payday Loan", "Traffic Cone", "Bone Zone", "Dial Tone", "Loud Trombone", "Public Telephone", "Blue Cheese Cologne", "Low Groan", "Leave Me Alone", "Jesus Atone", "Brutal Disown", "Incoming Cyclone", "Broken Collarbone", "Xylophone", "Foreknown Pheromone", "Dreary Monotone", "Broken Hipbone", "Biaural Microphone", "Saw Your Sister In The Cornerstone", "Release Postponed"]],
+            ["Bluehole's", ["Bridge Troll", "Remote Control"], ["Pigeonhole", "Beanpole", "Cubbyhole", "Screwhole", "Loo Roll", "Soul", "Collection of Foals", "Bowl", "Casserole", "Glycerol", "Bedroll", "Profiterole", "Buttonhole", "Morning Stroll"]],
+            ["Plunder Under's", ["Battle Grunder", "Wonder Lumber", "Battle Wonder", "Bumper Plunger", "Hunter Thunder", "Enourmous Thunder", "Number Blunder", "Car Sunder", "Udder Blunder", "Sulphur Slumber"]]
+        ],
+        player_alternatives: [
+            "Player", "Comrade", "Participant", "Contestant", "Thespian", "Popular", "Performer"
+        ],
+        unknown_alternatives: [
+            ["Unknown", p(false)], ["Nameless", p(true)], ["Unidentified", p(true)], ["Obscure", r()], ["Unsung", p(true)], ["Inglorious", p(true)], ["Unfamiliar", r()]
+        ],
+        battle_alternatives: [
+            "Battle", "Ideology", "Cookware", "Punchfight", "Conflict", "Fight", "Engagement", "Struggle", "Combat", "Beetle", "Bottle"
+        ],
+        grounds_alternatives: [
+            "Grounds", "Dustup", "Island", "Yard", "Field", "Parcel", "Sediment", "Quarry"
+        ],
+        full_names: [
+            "Secret Player", "Unnamed Person", "Mystery Guy", "Brendan Greene"
+        ],
+        full_suffixes: [
+            "Frank Exchange of Bullets", "All-Caps Gunplay"
+        ]
+    }
+
+    // Generators take no arguments, they simply produce a result for a pattern.
+    var generators = {
+        rhymepair: function() {
+            var pair = randomFrom(pools.pairs)
+
+            return pair.map(function(item) {
+                if (Array.isArray(item)) {
+                    return randomFrom(item)
+                } else {
+                    return item
+                }
+            }).join(" ")
+        },
+        name_twopart: function() {
+            var prefix = randomFrom(pools.player_alternatives)
+            var suffixData = randomFrom(pools.unknown_alternatives)
+            var suffix = suffixData[0];
+
+            if (suffixData[1]()) { // if true, swap the pairs
+                suffix = prefix;
+                prefix = suffixData[0];
+            }
+
+            return [prefix, suffix].join(" ");
+        },
+        name: function() {
+            if (r()()) {
+                return this.name_twopart()
+            } else {
+                return randomFrom(pools.full_names)
+            }
+        },
+        place_twopart: function() {
+            var prefix = randomFrom(pools.battle_alternatives)
+            var suffix = randomFrom(pools.grounds_alternatives)
+
+            return [prefix, suffix].join(" ")
+        },
+        place: function() {
+            if (r(0.2)()) {
+                return randomFrom(pools.full_suffixes)
+            } else {
+                return this.place_twopart()
+            }
+        }
+    }
+
+    var partsPattern = [
+        { type: "GENERATOR", name: "name" },
+        { type: "RAW", text: "'s " },
+        { type: "GENERATOR", name: "place" }
+    ]
+
+    var rhymingPattern = [
+        { type: "GENERATOR", name: "rhymepair" }
+    ]
+
+    function generate(pattern) {
+        return pattern.map(function(item) {
+            if (item.type == "GENERATOR") {
+                return generators[item.name]()
+            } else if (item.type == "RAW") {
+                return item.text
+            }
+        }).join("")
+    }
+
+    function displayName(text) {
+        ele.innerHTML = text;
+    }
+
+    window.generate = function() {
+        if (r()()) {
+            displayName(generate(rhymingPattern))
+        } else {
+            displayName(generate(partsPattern))
+        }
+    }
+
+    window.generate();
+})();
