a444f7390fd5856543abdc89ab6d35d076f95665
Henry <omgredstone@gmail.com>
2017-12-01 10:41:34 +0000

    Changed the rhyming generator; removed the pre-written pairs for a more random system

--- static/index.js
+++ static/index.js
@@ -5,6 +5,15 @@
         return list[Math.floor(Math.random() * list.length)]
     }
 
+    function e(times, cb) {
+        var results = [];
+        for (var i = 0; i < times; i++) {
+            results.push(cb());
+        }
+
+        return results
+    }
+
     function p(val) {
         return function() {
             return val
@@ -19,46 +28,44 @@
     }
 
     var pools = {
-        pairs: [
-            ["Brendan Greene's", ["Magic Machine", "Fight Ravine", "Sideways Lean", "Shine n' Gleam", "Coming Clean", "Magical Bean", "Bug Screen", "Gaming Scene", "Have You Seen?", "Party Bean", "Subteen Houseclean", "Spilled Gasoline", "Unseen Evergreen", "Peregrine Wolverine", "Porno Magazine", "Sweet Sixteen", "Unclean Submarine", "Supreme Regime"]],
-            ["Brenden Grenden's", ["Battle Grenden", "British Referendum", "Increased Momentum", "Physics Intervention", "Stretched Tendon", "Opposite Intention", "Anti-Cheat Circumvention", "Nonintervention", "Cooking Inattention", "Score Correction", "Genius Invention", "Tax Exemption", "Seeking Attention", "School Detention", "Furious Contention", "Anime Collection", "Coupon Redemption", "Lawyer Objection", "Reading Comprehension", "Traffic Congestion", "Delicious Confection", "Explosive Decompression", "Geneva Convention", "Contraception Imperfection", "Variable Indentation", "Specific Affection", "Hessian Reception", "Fair Complexion", "Engine Dimension", "Facial Expression", "Faulty Recollection"]],
-            ["Playerunknown's", ["Daily Moan", "Cordless Phone", "Gunplay Zone", "Payday Loan", "Traffic Cone", "Bone Zone", "Dial Tone", "Loud Trombone", "Public Telephone", "Blue Cheese Cologne", "Low Groan", "Leave Me Alone", "Jesus Atone", "Brutal Disown", "Incoming Cyclone", "Broken Collarbone", "Xylophone", "Foreknown Pheromone", "Dreary Monotone", "Broken Hipbone", "Biaural Microphone", "Saw Your Sister In The Cornerstone", "Release Postponed"]],
-            ["Bluehole's", ["Bridge Troll", "Remote Control"], ["Pigeonhole", "Beanpole", "Cubbyhole", "Screwhole", "Loo Roll", "Soul Hole", "Collection of Foals", "Bowl", "Casserole", "Glycerol", "Bedroll", "Profiterole", "Buttonhole", "Morning Stroll"]],
-            ["Plunder Under's", ["Battle Grunder", "Wonder Lumber", "Battle Wonder", "Bumper Plunger", "Hunter Thunder", "Enourmous Thunder", "Number Blunder", "Car Sunder", "Udder Blunder", "Sulphur Slumber", "Asunder Plumber", "Midsummer Thunder", "Runner Gunner", "Rubber Drummer"]],
-            ["Brendan Booty's", ["Shooty Looty", "Jury Duty", "Tutti Fruity", "Cutie Beauty", "Droopy Cootie", "Snooty Ruby"]]
+        rhymes: [
+            ["Brendan Greene", ["Machine", "Ravine", "Lean", "Gleam", "Clean", "Bean", "Sean", "Screen", "Scene", "Seen", "Bean", "Subteen", "Preteen", "Houseclean", "Gasoline", "Unseen", "Evergreen", "Peregrine", "Wolverine", "Magazine", "Sixteen", "Unclean", "Submarine", "Supreme", "Reigime"]],
+            ["Brenden Grenden", ["Grenden", "Referendum", "Momentum", "Intervention", "Tendon", "Intention", "Circumvention", "Nonintervention", "Inattention", "Correction", "Invention", "Exemption", "Attention", "Detention", "Contention", "Collection", "Redemption", "Objection", "Comprehension", "Confection", "Decompression", "Convention", "Contraception", "Imperfection", "Indentation", "Affection", "Reception", "Complexion", "Dimension", "Expression", "Recollection"]],
+            ["PLAYERUNKNOWN", ["Moan", "Phone", "Zone", "Loan", "Cone", "Tone", "Trombone", "Telephone", "Cologne", "Groan", "Alone", "Atone", "Disown", "Cyclone", "Collarbone", "Xylophone", "Foreknown", "Pheromone", "Monotone", "Hipbone", "Microphone", "Cornerstone", "Postponed"]],
+            ["Bluehole", ["Bridge Troll", "Control", "Pigeonhole", "Beanpole", "Pole", "Cubbyhole", "Screwball", "Screwhole", "Roll", "Hole", "Foal", "Bowl", "Casserole", "Glycerol", "Bedroll", "Profiterole", "Buttonhole", "Stroll"]],
+            ["Plunder Under", ["Grunder", "Wonder", "Lumber", "Bumper", "Plunger", "Hunter", "Thunder", "Number", "Blunder", "Sunder", "Slumber", "Plumber", "Midsummer", "Runner", "Gunner", "Drummer"]],
+            ["Brendan Booty", ["Shooty", "Looty", "Duty", "Tutti", "Fruity", "Cutie", "Beauty", "Droopy", "Ruby", "Snooty", "Loopy"]]
         ],
         player_alternatives: [
-            "Player", "Comrade", "Participant", "Contestant", "Thespian", "Popular", "Performer"
+            "Player", "Comrade", "Participant", "Contestant", "Thespian", "Popular", "Performer", "Chef", "Soldier", "Baker", "General", "Wanderer", "Loner"
         ],
         unknown_alternatives: [
-            ["Unknown", p(false)], ["Nameless", p(true)], ["Unidentified", p(true)], ["Obscure", r()], ["Unsung", p(true)], ["Inglorious", p(true)], ["Unfamiliar", r()]
+            ["Unknown", p(false)], ["Nameless", p(true)], ["Unidentified", p(true)], ["Obscure", r()], ["Unsung", p(true)], ["Inglorious", p(true)], ["Unfamiliar", r()], ["Infamous", p(true)], ["Anonymous", p(true)], ["Uncommon", r()]
         ],
         battle_alternatives: [
-            "Battle", "Ideology", "Cookware", "Punchfight", "Conflict", "Fight", "Engagement", "Struggle", "Combat", "Beetle", "Bottle"
+            "Battle", "Ideology", "Cookware", "Punchfight", "Conflict", "Fight", "Engagement", "Struggle", "Combat", "Beetle", "Bottle", "Zombie", "Gunplay", "Death"
         ],
         grounds_alternatives: [
-            "Grounds", "Dustup", "Island", "Yard", "Field", "Parcel", "Sediment", "Quarry"
+            "Grounds", "Dustup", "Island", "Yard", "Field", "Parcel", "Sediment", "Quarry", "Kitchen", "Laboratory", "School", "Office", "Plains", "Mountain"
         ],
         full_names: [
             "Secret Player", "Unnamed Person", "Mystery Guy", "Brendan Greene"
         ],
         full_suffixes: [
-            "Frank Exchange of Bullets", "All-Caps Gunplay"
+            "Frank Exchange of Bullets", "All-Caps Gunplay", "Shooty Shooty Bang Bang", "Dying to Death Simulator", "Zombie Island"
         ]
     }
 
     // Generators take no arguments, they simply produce a result for a pattern.
     var generators = {
         rhymepair: function() {
-            var pair = randomFrom(pools.pairs)
+            var slots = randomFrom(pools.rhymes);
+            var prefix = slots[0] + "'s";
+            var suffix = e(2, function() {
+                return randomFrom(slots[1])
+            }).join(" ");
 
-            return pair.map(function(item) {
-                if (Array.isArray(item)) {
-                    return randomFrom(item)
-                } else {
-                    return item
-                }
-            }).join(" ")
+            return [prefix, suffix].join(" ");
         },
         name_twopart: function() {
             var prefix = randomFrom(pools.player_alternatives)
